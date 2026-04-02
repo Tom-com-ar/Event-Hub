1747dece229cf969e01a2edea115e84cb17d4d0a
Tom-com-ar <tomasiezzi16@gmail.com>
2026-04-01 21:08:55 -0300

    Links de los Header

--- includes/header.php
+++ includes/header.php
@@ -2,17 +2,17 @@
     <div class="top-bar-left">
         <ul class="dropdown menu" data-dropdown-menu>
             <li class="menu-text">Mi Sitio Moderno</li>
-            <li><a href="index.php">Inicio</a></li>
-            <li><a href="pages/perfil.php">Perfil</a></li>
+            <li><a href="/Event-Hub/index.php">Inicio</a></li>
+            <li><a href="/Event-Hub/pages/perfil.php">Perfil</a></li>
         </ul>
     </div>
     <div class="top-bar-right">
         <ul class="menu">
             <li><a href="#" id="toggleDarkMode"><i class="fas fa-moon"></i> Modo Oscuro</a></li>
             <?php if (isset($_SESSION['usuario'])): ?>
-                <li><a href="backend/logout.php">Cerrar Sesión</a></li>
+                <li><a href="/Event-Hub/backend/logout.php">Cerrar Sesión</a></li>
             <?php else: ?>
-                <li><a href="pages/login.php">Iniciar Sesión</a></li>
+                <li><a href="/Event-Hub/pages/login.php">Iniciar Sesión</a></li>
             <?php endif; ?>
         </ul>
     </div>
